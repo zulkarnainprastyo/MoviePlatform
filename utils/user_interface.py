f26e766752028fdbc060fad3b33265aef566b104
zulkarnainprastyo <zulkarnain.prastyo@binus.ac.id>
2023-09-10 12:34:31 +0700

    update all

--- utils/user_interface.py
+++ utils/user_interface.py
@@ -1,47 +1,63 @@
-import sys
-sys.path.append(r"C:\ProjectRecommendationSystems\MoviePlatform\utils")
+# utils/user_interface.py
 
-import pandas as pd
-from models.collaborative_filtering import train_collaborative_filtering_model, get_collaborative_filtering_recommendations
-from utils.data_preprocessing import preprocess_user_input
+class UserInterface:
+    def __init__(self, collaborative_filtering_model, content_based_model, deep_learning_model):
+        self.cf_model = collaborative_filtering_model
+        self.cb_model = content_based_model
+        self.dl_model = deep_learning_model
 
-def create_user_interface(collaborative_model):
-    while True:
-        print("\nMovie Recommendation System")
-        print("1. Get Movie Recommendations")
-        print("0. Exit")
+    def get_user_input(self):
+        print("Welcome to Movie Recommendation System!")
+        while True:
+            try:
+                user_id = int(input("Enter your user ID: "))
+                if user_id > 0:
+                    return user_id
+                else:
+                    print("Invalid user ID. Please enter a positive integer.")
+            except ValueError:
+                print("Invalid input. Please enter a valid user ID (a positive integer).")
 
-        choice = input("Enter your choice: ")
+    def display_recommendations(self, user_id):
+        print("\nRecommendations for User", user_id)
+        
+        # Get recommendations from different models
+        recommendation_models = {
+            'Collaborative Filtering': self.cf_model,
+            'Content-Based': self.cb_model,
+            'Deep Learning': self.dl_model
+        }
+        
+        print("\nChoose a recommendation model:")
+        for i, model_name in enumerate(recommendation_models.keys(), start=1):
+            print(f"{i}. {model_name}")
+        
+        while True:
+            try:
+                model_choice = int(input("Enter the number of your preferred model: "))
+                if 1 <= model_choice <= len(recommendation_models):
+                    chosen_model = list(recommendation_models.values())[model_choice - 1]
+                    break
+                else:
+                    print("Invalid choice. Please enter a valid number.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+        
+        recommendations = chosen_model.get_recommendations(user_id)
+        
+        print(f"\n{chosen_model.__class__.__name__} Recommendations:")
+        for i, movie in enumerate(recommendations, start=1):
+            print(f"{i}. {movie}")
 
-        if choice == '1':
-            user_id = input("Enter your user ID: ")
+    def run(self):
+        user_id = self.get_user_input()
+        self.display_recommendations(user_id)
 
-            # Ensure user ID is valid and preprocess it if needed
-            user_id = preprocess_user_input(user_id)
-
-            if user_id is None:
-                print("Invalid user ID. Please try again.")
-                continue
-
-            # Get collaborative filtering recommendations
-            recommendations = get_collaborative_filtering_recommendations(collaborative_model, user_id, top_n=10)
-
-            # Display recommendations
-            print("\nTop Recommendations:")
-            for i, movie in enumerate(recommendations, start=1):
-                print(f"{i}. {movie}")
-
-        elif choice == '0':
-            print("Goodbye!")
-            break
-
-        else:
-            print("Invalid choice. Please select a valid option.")
-
-if __name__ == "__main__":
-    # Initialize your collaborative filtering model here
-    user_ratings = pd.read_csv('user_ratings.csv')  # Load your user ratings data
-    collaborative_model = train_collaborative_filtering_model(user_ratings)
-
-    # Start the user interface
-    create_user_interface(collaborative_model)
+# Example Usage:
+# if __name__ == '__main__':
+#     collaborative_filtering_model = CollaborativeFilteringModel()
+#     content_based_model = ContentBasedModel()
+#     deep_learning_model = DeepLearningModel()
+#
+#     ui = UserInterface(collaborative_filtering_model, content_based_model, deep_learning_model)
+#     ui.run()
--- utils/user_interface_script.py
+++ utils/user_interface_script.py
@@ -1,16 +1,54 @@
-def main():
-    user_ratings = load_user_ratings('C:\ProjectRecommendationSystems\MoviePlatform\data/user_ratings.csv')
-    collaborative_filtering_model = train_collaborative_filtering_model(user_ratings)
+class UserInterface:
+    def __init__(self):
+        self.user_id = None
 
-    while True:
-        user_id = input("Enter user ID (or 'q' to quit): ")
-        if user_id.lower() == 'q':
-            break
-        
-        recommendations = get_collaborative_filtering_recommendations(collaborative_filtering_model, user_id)
-        print(f"Top Recommendations for User {user_id}:")
-        for movie, score in recommendations:
-            print(f"Movie ID: {movie}, Score: {score:.2f}")
+    def display_menu(self):
+        print("Main Menu:")
+        print("1. Option 1")
+        print("2. Option 2")
+        print("3. Exit")
+
+    def get_user_input(self):
+        return input("Enter your choice: ")
+
+    def get_user_id(self):
+        while True:
+            try:
+                user_id = int(input("Enter User ID: "))
+                return user_id
+            except ValueError:
+                print("Invalid User ID. Please enter a valid integer.")
+
+    def option_1(self):
+        if self.user_id is not None:
+            print(f"Option 1 selected for User ID {self.user_id}")
+        else:
+            print("Option 1 selected.")
+
+    def option_2(self):
+        if self.user_id is not None:
+            print(f"Option 2 selected for User ID {self.user_id}")
+        else:
+            print("Option 2 selected.")
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = self.get_user_input()
+            if choice == "1":
+                self.option_1()
+            elif choice == "2":
+                self.option_2()
+            elif choice == "3":
+                print("Exiting the program.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+    def main(self):
+        self.user_id = self.get_user_id()
+        self.run()
 
 if __name__ == "__main__":
-    main()
+    ui = UserInterface()
+    ui.main()
